33e68338b619adfa87da6aa8a09513b8984af9df
heba-ali2030 <heba.galaa@gmail.com>
2021-02-06 03:24:35 +0200

    add

---
+++ medical_students.py
@@ -0,0 +1,9 @@
+# please answer the following quistions
+# it ranged from 0(bad) to 5(excellent)
+name =input('my name is ')
+faculty = input('my faculty is ')
+q1 = input('do you find this subject is interesting?')
+q2 = input('is it related to the field? ')
+q3 = input(' give a percent for internet services used : ')
+q4 = input(' give a percent for session interactions : ')
+print(' my total satisfaction is {q3+q4}')
